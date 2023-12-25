4c01d95f496d0d5fc313223e7ac3e769a3b18abd
liasovIhsan04 <liasovihsan1@gmail.com>
2023-12-25 17:41:19 +0600

    ihsan

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FiUser } from "react-icons/fi";
 import { BiBell } from "react-icons/bi";
 import bekbekei from "../img/bekbekei-h-logo.svg";
@@ -7,12 +7,30 @@ import "../style/css/media.css";
 import "../style/css/main.css";
 import { HiArrowLongLeft } from "react-icons/hi2";
 import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { auth } from "../Redux/reduser/auth";
 
-const Header = ({ user }) => {
+const Header = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const token = localStorage.getItem("token")
     ? localStorage.getItem("token")
     : false;
+
+  const data = useSelector((state) => state.users);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        await dispatch(auth());
+      } catch (error) {
+        console.error("Ошибка инициализации PullToRefresh:", error);
+      }
+    };
+
+    fetchData();
+  }, [dispatch]);
+  console.log(data);
 
   return (
     <div>
@@ -25,7 +43,8 @@ const Header = ({ user }) => {
           />
           {token ? (
             <h2 className="first_last_name_header" style={{ color: "#fff" }}>
-              Привет, {user.first_name} {user.last_name}
+              Привет, {data && data.user.first_name}{" "}
+              {data && data.user.last_name}
             </h2>
           ) : (
             <img src={bekbekei} alt="" />
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -13,65 +13,102 @@ import classNames from "classnames";
 import logo from "../img/bekbekei-logotip.png";
 import ReactPullToRefresh from "react-pull-to-refresh";
 import NewPromotions from "../pages/NewPromotions";
+import axios from "axios";
+import { url } from "../Api";
+import { auth } from "../Redux/reduser/auth";
+import { useDispatch, useSelector } from "react-redux";
+import LoadingPullTo from "../UI/LoadingPullTo/LoadingPullTo";
 
-const Main = ({ user }) => {
+const Main = () => {
+  const [user, setUser] = useState([]);
+  const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const [refreshing, setRefreshing] = useState(false);
   const token = localStorage.getItem("token")
     ? localStorage.getItem("token")
     : false;
   console.log(user);
+  const [local, setLocal] = useState(localStorage.getItem("tokens"));
+  const headers = {
+    Authorization: `Token ${local}`,
+  };
+  const data = useSelector((state) => state.users);
+
+  const initPullToRefresh = async () => {
+    try {
+      await dispatch(auth());
+    } catch (error) {
+      console.error("Ошибка инициализации PullToRefresh:", error);
+    } finally {
+      setRefreshing(false);
+    }
+  };
+
+  useEffect(() => {
+    initPullToRefresh();
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (refreshing) {
+      initPullToRefresh();
+    }
+  }, [refreshing]);
 
   const handleRefresh = () => {
-    window.location.reload(user);
+    setRefreshing(true);
   };
 
   return (
-    <ReactPullToRefresh onRefresh={handleRefresh}>
-      <div className="main-block">
-        <Header user={user} />
-        <div className="margin_top">
-          <div className="container">
-            <Storis token={token} />
+    <div className="main-block">
+      <Header user={user} />
+        <ReactPullToRefresh onRefresh={handleRefresh}>
+          <div style={{display: "flex", justifyContent: "center"}}>
+          <LoadingPullTo />
           </div>
-        </div>
-        {/* <SwipeableBottomSheet overflowHeight={574}> */}
-        <div className="swiper">
-          <div className="container">
-            {token ? (
-              <div style={{ padding: "20px 0 0 0" }}>
-                <div className="margin_top_all">
-                  <div className="margin_top_log">
-                    <DiVisualstudio className="dio" />
-                    <img src={logo} alt="" className="dio" />
+          <div className="margin_top">
+            <div className="container">
+              <Storis token={token} />
+            </div>
+          </div>
+          {/* <SwipeableBottomSheet overflowHeight={574}> */}
+          <div className="swiper">
+            <div className="container">
+              {token ? (
+                <div style={{ padding: "20px 0 0 0" }}>
+                  <div className="margin_top_all">
+                    <div className="margin_top_log">
+                      <DiVisualstudio className="dio" />
+                      <img src={logo} alt="" className="dio" />
+                    </div>
+                  </div>
+                  <div
+                    className="bonus_block_all"
+                    onClick={() => navigate("/qr-cod")}
+                  >
+                    <div>
+                      <h4 className="title_h">Бонусная карта</h4>
+                      <h1 className="title_ha">
+                        {data.user.bonus}
+                        <span>бонусов</span>
+                      </h1>
+                    </div>
+                    <div className="bonus_img">
+                      <img src={data.user.qrimg} alt="" />
+                    </div>
                   </div>
                 </div>
-                <div
-                  className="bonus_block_all"
-                  onClick={() => navigate("/qr-cod")}
-                >
-                  <div>
-                    <h4 className="title_h">Бонусная карта</h4>
-                    <h1 className="title_ha">
-                      {user.bonus}
-                      <span>бонусов</span>
-                    </h1>
-                  </div>
-                  <div className="bonus_img">
-                    <img src={user.qrimg} alt="" />
-                  </div>
-                </div>
-              </div>
-            ) : (
-              ""
-            )}
+              ) : (
+                ""
+              )}
+            </div>
+            <GetShot token={token} user={user} />
+            <NewPromotions />
+            {/* <MyCoupon />   */}
           </div>
-          <GetShot token={token} user={user} />
-          <NewPromotions />
-          {/* <MyCoupon />   */}
-        </div>
-        {/* </SwipeableBottomSheet> */}
-      </div>
-    </ReactPullToRefresh>
+          {/* </SwipeableBottomSheet> */}
+        </ReactPullToRefresh>
+    </div>
   );
 };
 
